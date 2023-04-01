fa548b3db5e55fe29b9f9cdaf57704f46dae0fd0
cam124 <69061719+cam124@users.noreply.github.com>
2023-04-01 15:42:33 -0400

    added api key to homepage to test

--- website/views.py
+++ website/views.py
@@ -12,7 +12,7 @@ openai.api_key = API_KEY
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', API_KEY)
 
 @views.route('/author')
 def author():
